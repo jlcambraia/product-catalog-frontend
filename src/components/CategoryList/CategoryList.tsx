1ea89865551d30115d6cf10a54479662676e2f7f
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-06-25 14:09:03 -0300

    feat: create component CategoryList

---
+++ src/components/CategoryList/CategoryList.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import styles from "./CategoryList.module.css";
+import CategoryCard from "./components/CategoryCard";
+
+import { useEffect, useState } from "react";
+
+import { Category, CategoryProps } from "@/types/types";
+
+const CategoryList = ({
+  handleFilterByCategory,
+  activeCategory,
+  allProducts,
+}: CategoryProps) => {
+  const [category, setCategory] = useState<Category[]>([]);
+
+  useEffect(() => {
+    if (!allProducts) return;
+
+    const allCategories = allProducts
+      .map((product) => product.category)
+      .filter((cat, index, arr) => arr.indexOf(cat) === index);
+
+    const sortedCategories = allCategories.sort((a, b) => {
+      if (a === "OUTROS") return 1;
+      if (b === "OUTROS") return -1;
+      return a.localeCompare(b);
+    });
+
+    setCategory(sortedCategories);
+  }, [allProducts]);
+
+  return (
+    <ul className={styles.list}>
+      <CategoryCard
+        item="Todos"
+        handleFilterByCategory={handleFilterByCategory}
+        isActive={activeCategory === "Todos"}
+      />
+      {category.map((item) => (
+        <CategoryCard
+          key={item}
+          item={item}
+          handleFilterByCategory={handleFilterByCategory}
+          isActive={activeCategory === item}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default CategoryList;
