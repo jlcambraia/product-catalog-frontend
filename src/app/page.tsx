eb496fd947f95b284dd118504ea6c1d24000d285
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-06-25 14:07:56 -0300

    feat: add page

---
+++ src/app/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import styles from "./page.module.css";
+import Header from "@/components/Header/Header";
+import CategoryList from "@/components/CategoryList/CategoryList";
+import ProductsList from "@/components/ProductsList/ProductsList";
+import Footer from "@/components/Footer/Footer";
+import { useState } from "react";
+import { productsMock } from "@/data/mocks/productsMock";
+import { ProductsInterface, Category } from "@/types/types";
+
+export default function Home() {
+  const [products, setProducts] = useState<ProductsInterface[]>(productsMock);
+  const [allProducts, setAllProducts] =
+    useState<ProductsInterface[]>(productsMock);
+  const [activeCategory, setActiveCategory] = useState<string>("Todos");
+  const [isLikedList, setIsLikedList] = useState<ProductsInterface[]>([]);
+
+  const handleFilterByCategory = (category: string | Category): void => {
+    setActiveCategory(category.toString());
+
+    if (category === "Todos") {
+      setProducts(allProducts);
+      return;
+    }
+
+    const filteredProducts = allProducts.filter(
+      (product) => product.category === category
+    );
+
+    setProducts(filteredProducts);
+  };
+
+  return (
+    <>
+      <div className={styles.container}>
+        <Header
+          products={products}
+          setProducts={setProducts}
+          setAllProducts={setAllProducts}
+          setActiveCategory={setActiveCategory}
+          isLikedList={isLikedList}
+        />
+        <main className={styles.main}>
+          <CategoryList
+            handleFilterByCategory={handleFilterByCategory}
+            activeCategory={activeCategory}
+            allProducts={allProducts}
+          />
+          <ProductsList
+            products={products}
+            isLikedList={isLikedList}
+            setIsLikedList={setIsLikedList}
+          />
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+}
