5869f45321a1e15c741fb72425fab5da74bb9b50
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-06-25 14:09:10 -0300

    feat: create component CategoryCard

---
+++ src/components/CategoryList/components/CategoryCard.tsx
@@ -0,0 +1,26 @@
+import styles from "./CategoryCard.module.css";
+import { CategoryProps } from "@/types/types";
+
+const CategoryCard = ({
+  item = "Todos",
+  handleFilterByCategory,
+  isActive,
+}: CategoryProps) => {
+  const showCategory = (item: string): string => {
+    return item[0].toUpperCase() + item.slice(1).toLowerCase();
+  };
+
+  return (
+    <li
+      className={`
+        ${styles.card}
+        ${isActive ? styles.cardActive : ""}
+      `}
+      onClick={() => handleFilterByCategory(item)}
+    >
+      <p>{showCategory(item)}</p>
+    </li>
+  );
+};
+
+export default CategoryCard;
