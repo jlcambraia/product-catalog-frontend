6ee467735f1c3698bf2a967224594fde63593fd0
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-07-11 11:26:12 -0300

    feat: create class Api

---
+++ src/utils/api/Api.js
@@ -0,0 +1,72 @@
+class Api {
+	constructor({ baseUrl, headers, makeRequest }) {
+		this._baseUrl = baseUrl;
+		this._headers = headers;
+		this._makeRequest = makeRequest;
+	}
+
+	_handleResponse(res) {
+		return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+	}
+
+	async getProducts() {
+		const res = await this._makeRequest(`${this._baseUrl}/products`, {
+			method: 'GET',
+			headers: this._headers,
+		});
+		return await this._handleResponse(res);
+	}
+
+	async createProduct(product) {
+		const newProduct = await this._makeRequest(`${this._baseUrl}/products`, {
+			method: 'POST',
+			headers: this._headers,
+			body: JSON.stringify({
+				name: product.name,
+				price: product.price,
+				description: product.description,
+				image: product.image,
+				category: product.category,
+				stock: product.stock,
+			}),
+		});
+		return await this._handleResponse(newProduct);
+	}
+
+	async deleteProduct(productId) {
+		const deletedProduct = await this._makeRequest(
+			`${this._baseUrl}/products/${productId}`,
+			{
+				method: 'DELETE',
+				headers: this._headers,
+			}
+		);
+		return await this._handleResponse(deletedProduct);
+	}
+
+	async toggleLikeProduct(productId, currentIsLiked) {
+		const toggleLike = await this._makeRequest(
+			`${this._baseUrl}/products/${productId}`,
+			{
+				method: 'PATCH',
+				headers: this._headers,
+				body: JSON.stringify({
+					isLiked: !currentIsLiked,
+				}),
+			}
+		);
+		return await this._handleResponse(toggleLike);
+	}
+}
+
+const apiConfig = {
+	baseUrl: process.env.NEXT_PUBLIC_MAINAPI_BASE_URL,
+	headers: {
+		'Content-Type': 'application/json',
+	},
+	makeRequest: (...args) => fetch(...args),
+};
+
+const api = new Api(apiConfig);
+
+export default api;
