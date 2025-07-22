e6cc116524fe1b49e2b26aeaef9aadfb13914c2a
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-07-22 13:01:38 -0300

    refactor: retira validação, que agora está no validators.ts

--- src/components/CategoryList/components/CategoryCard.tsx
+++ src/components/CategoryList/components/CategoryCard.tsx
@@ -1,15 +1,12 @@
 import styles from './CategoryCard.module.css';
 import { CategoryProps } from '@/types/types';
+import { showCategory } from '@/utils/validators/validators';
 
 const CategoryCard = ({
 	item = 'Todos',
 	handleFilterByCategory,
 	isActive,
 }: CategoryProps) => {
-	const showCategory = (item: string): string => {
-		return item[0].toUpperCase() + item.slice(1).toLowerCase();
-	};
-
 	return (
 		<li
 			className={`
